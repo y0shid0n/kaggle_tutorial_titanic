ff9efb95bc1ae5f4a443dd2dc6a2e8ff218a0e63
NaohiroYoshida <y0shid0n1031@gmail.com>
2020-05-31 19:04:21 +0900

    add svc

--- stacking.py
+++ stacking.py
@@ -4,27 +4,38 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 from datetime import datetime
 
+# hash values
+hash_lr = "5263dece31afd0beb01a03b3e3a3731c"
+hash_rf = "1bef2dfa990cd12bd8ad4313d2830030"
+hash_knn = "d8718b7844718dc4490ea3a75bc6c2bd"
+hash_lgbm = "f9a7f16ed061fbefb81b345d331d60b9"
+hash_catb = "42de8481ab2b6892b64d9c8b9f931821"
+hash_svc = "eef8fe77878c1c6c947cbe8d7b23757a"
+
 # ベースモデル読み込み
-with open("./pkl/lr_5263dece31afd0beb01a03b3e3a3731c.pkl", "rb") as f:
+with open(f"./pkl/lr_{hash_lr}.pkl", "rb") as f:
     clf_lr = pickle.load(f)
-with open("./pkl/rf_3cf155877177efa6771973aa0c5b8059.pkl", "rb") as f:
+with open(f"./pkl/rf_{hash_rf}.pkl", "rb") as f:
     clf_rf = pickle.load(f)
-with open("./pkl/knn_891c09c3095237ef5e270a8efd5df9d3.pkl", "rb") as f:
+with open(f"./pkl/knn_{hash_knn}.pkl", "rb") as f:
     clf_knn = pickle.load(f)
-with open("./pkl/lgbm_f9a7f16ed061fbefb81b345d331d60b9.pkl", "rb") as f:
+with open(f"./pkl/lgbm_{hash_lgbm}.pkl", "rb") as f:
     clf_lgbm = pickle.load(f)
-with open("./pkl/catb_42de8481ab2b6892b64d9c8b9f931821.pkl", "rb") as f:
+with open(f"./pkl/catb_{hash_catb}.pkl", "rb") as f:
     clf_catb = pickle.load(f)
+with open(f"./pkl/svc_{hash_svc}.pkl", "rb") as f:
+    clf_svc = pickle.load(f)
 
 # データ読み込み
 df_test_tree = pd.read_csv("./data/test_prep_tree.csv")
 df_test_nontree = pd.read_csv("./data/test_prep_nontree.csv")
 
-df_lr = pd.read_csv("./output/base_pred_proba_lr_5263dece31afd0beb01a03b3e3a3731c.csv")
-df_rf = pd.read_csv("./output/base_pred_proba_rf_3cf155877177efa6771973aa0c5b8059.csv")
-df_knn = pd.read_csv("./output/base_pred_proba_knn_891c09c3095237ef5e270a8efd5df9d3.csv")
-df_lgbm = pd.read_csv("./output/base_pred_proba_lgbm_f9a7f16ed061fbefb81b345d331d60b9.csv")
-df_catb = pd.read_csv("./output/base_pred_proba_catb_42de8481ab2b6892b64d9c8b9f931821.csv")
+df_lr = pd.read_csv(f"./output/base_pred_proba_lr_{hash_lr}.csv")
+df_rf = pd.read_csv(f"./output/base_pred_proba_rf_{hash_rf}.csv")
+df_knn = pd.read_csv(f"./output/base_pred_proba_knn_{hash_knn}.csv")
+df_lgbm = pd.read_csv(f"./output/base_pred_proba_lgbm_{hash_lgbm}.csv")
+df_catb = pd.read_csv(f"./output/base_pred_proba_catb_{hash_catb}.csv")
+df_svc = pd.read_csv(f"./output/base_pred_proba_svc_{hash_svc}.csv")
 
 df_all = df_lr.rename(columns={"pred": "lr"})\
     .merge(df_rf, on=["PassengerId", "Survived"], how="inner")\
@@ -34,7 +45,9 @@ df_all = df_lr.rename(columns={"pred": "lr"})\
     .merge(df_lgbm, on=["PassengerId", "Survived"], how="inner")\
     .rename(columns={"pred": "lgbm"})\
     .merge(df_catb, on=["PassengerId", "Survived"], how="inner")\
-    .rename(columns={"pred": "catb"})
+    .rename(columns={"pred": "catb"})\
+    .merge(df_svc, on=["PassengerId", "Survived"], how="inner")\
+    .rename(columns={"pred": "svc"})
 
 # stack base predicts for training meta model
 X_valid = df_all.drop(["PassengerId", "Survived"], axis=1)
@@ -54,8 +67,9 @@ pred_knn = clf_knn.predict_proba(X_test_nontree)[:, 1]
 pred_lgbm = clf_lgbm.predict(X_test_tree)  # return probability
 #pred_lgbm = [0 if i < 0.5 else 1 for i in pred_lgbm]
 pred_catb = clf_catb.predict_proba(X_test_tree)[:, 1]
+pred_svc = clf_svc.predict_proba(X_test_nontree)[:, 1]
 
-stacked_preds = np.column_stack((pred_lr, pred_rf, pred_knn, pred_lgbm, pred_catb))
+stacked_preds = np.column_stack((pred_lr, pred_rf, pred_knn, pred_lgbm, pred_catb, pred_svc))
 meta_valid_pred = meta_model.predict(stacked_preds)
 
 # 出力
